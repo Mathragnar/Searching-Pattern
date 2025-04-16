848941fd713943fa75b3424f4df144d5ca94ff63
mathragnar <mathragnar@gmail.com>
2025-04-16 18:51:04 -0300

    Added two loops for

--- searching.py
+++ searching.py
@@ -1,8 +1,12 @@
 import csv
 
+pattern = []
 with open('data.csv', 'r') as file:
     reader = csv.reader(file)
     data = list(reader)
     # loop for each row in the file
     for row in data:
-        print(row)
+        #print(row)
+        pattern.append(row[0])
+    for i in pattern:
+        print(i)
