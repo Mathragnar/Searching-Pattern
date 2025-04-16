989343f0d0a0e097c3d7debd29e5a008551bc2fb
mathragnar <mathragnar@gmail.com>
2025-04-16 19:13:19 -0300

    Revert "Added two loops for"
    This reverts commit 848941fd713943fa75b3424f4df144d5ca94ff63.

--- searching.py
+++ searching.py
@@ -1,12 +1,8 @@
 import csv
 
-pattern = []
 with open('data.csv', 'r') as file:
     reader = csv.reader(file)
     data = list(reader)
     # loop for each row in the file
     for row in data:
-        #print(row)
-        pattern.append(row[0])
-    for i in pattern:
-        print(i)
+        print(row)
