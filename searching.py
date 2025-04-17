dd79b49451fd5f3068ef17023ee3237037ef6a39
mathragnar <mathragnar@gmail.com>
2025-04-16 23:19:23 -0300

    now loop with parameter enumerate and let out the [] using the loop for

--- searching.py
+++ searching.py
@@ -1,8 +1,15 @@
 import csv
 
+count = 0
+
 with open('data.csv', 'r') as file:
     reader = csv.reader(file)
     data = list(reader)
     # loop for each row in the file
     for row in data:
-        print(row)
+        #print(row)
+        for number, i in enumerate(row):
+            #print(number)
+            if number == row[i]:
+                print(f"Found {number} in row {row}")
+                count += 1
